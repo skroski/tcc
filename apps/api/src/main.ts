a551cabdbe6d06ad223ccb30edc9a6ca42296f8c
daniel.skroski <daniel@duplod.com.br>
2022-11-13 13:15:26 -0300

    Mudança do Swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('', app, document)
+    SwaggerModule.setup('api', app, document)
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
