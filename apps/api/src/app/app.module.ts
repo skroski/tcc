1d70fe62a60b21d053f0d655eacd126f9a4d52f6
daniel.skroski <daniel@duplod.com.br>
2023-03-02 08:01:48 -0300

    Correção de Pastas

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { BudgetsModule } from '../budgets/budgets.module';
-import { ClientsModule } from '../clients/clients.module';
-import { ServicesModule } from '../services/services.module';
-import { UsersModule } from '../users/users.module';
+import { BudgetsModule } from '../app/budgets/budgets.module';
+import { ClientsModule } from '../app/clients/clients.module';
+import { ServicesModule } from '../app/services/services.module';
+import { UsersModule } from '../app/users/users.module';
 
 @Module({
   imports: [                
@@ -14,8 +14,11 @@ import { UsersModule } from '../users/users.module';
         useFactory: async (config: ConfigService) => ({
           uri: config.get<string>('DATABASE_CONNECTION'),
       })
-    })
-    ,BudgetsModule, ServicesModule, UsersModule, ClientsModule,
+    }),
+    BudgetsModule, 
+    ServicesModule, 
+    UsersModule, 
+    ClientsModule,
     ],
    
 })
