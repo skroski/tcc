98694311591caedbd3a323a50a07970c453cb655
daniel.skroski <daniel@duplod.com.br>
2023-03-12 12:55:20 -0300

    Renew Dist to Fix

--- dist/apps/api/main.js
+++ dist/apps/api/main.js
@@ -1594,7 +1594,7 @@ function bootstrap() {
             .setVersion('1.0')
             .build();
         const document = swagger_1.SwaggerModule.createDocument(app, config);
-        swagger_1.SwaggerModule.setup('api', app, document);
+        swagger_1.SwaggerModule.setup('docs', app, document);
         const globalPrefix = 'api';
         app.setGlobalPrefix(globalPrefix);
         const port = process.env.PORT || 3333;
